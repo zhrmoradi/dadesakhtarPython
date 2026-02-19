569b6839e42493f6b14a63c34a81dbb53635b82c
zhrmoradi <zahra824264@gmail.com>
2026-02-19 13:55:55 +0330

    Update ds15.py

--- ds15.py
+++ ds15.py
@@ -10,14 +10,3 @@ F(n) = 3n**2 + 4n-8 ____ 4(n-2)
         b=c 
         c=d 
         d=e
-
-
-
-#for i in range(n):
-#    for i in range(n):
-#        p=k 
-#    t=s 
-
-#F(n) = n**2 + n        
-
-# n**2 به معنای (n) به توان دو
