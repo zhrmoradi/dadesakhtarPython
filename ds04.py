b0031733579733029091dce38af5ad4a149ff77a
zhrmoradi <zahra824264@gmail.com>
2026-02-19 13:31:28 +0330

    Update ds04.py

--- ds04.py
+++ ds04.py
@@ -1,112 +1,70 @@
-class C_Queue:
-    def __init__(self, max = 100):
-        self.list = [] * max
+class CircularQueue:
+    def __init__(self, capacity=100):
+        self.data = [None] * capacity
+        self.capacity = capacity
         self.front = -1
         self.rear = -1
-    def  insert(self , x):
-        if (self.rear +1) % len(self.list) == self.front:
-            print("Queue is full")
-            return
-        if  self.front == -1:
-            self.front = 0
-            self.rear = 0
-            self.list[0] = x
-            return
-        self.rear=(self.rear +1) % len(self.list)
-        self.list[self.rear] = x
-    def delete(self):
-        if self.front == -1:
-            print("Queue is empty")
-            return
-        if self.front == self.rear:
-            k = self.list[self.front]
-            self.front = -1
-            self.rear = -1
-            return k
-        k = self.list[self.front]
-        self.front = (self.front +1) % len (self.list)
-        return k
+
     def is_empty(self):
         return self.front == -1
-    
+
     def is_full(self):
-        return (self.rear +1) % len (self.list) == self.front
-    
-    def show_valid(self):
-        if self.rear >= self.front:
-            for i in range(self.front , self.rear+1 , 1):
-                print(self.list[i])
+        return (self.rear + 1) % self.capacity == self.front
+
+    def enqueue(self, value):
+        if self.is_full():
+            print("Queue is full")
+            return
+        if self.is_empty():
+            self.front = 0
+            self.rear = 0
+            self.data[self.rear] = value
+            return
+        self.rear = (self.rear + 1) % self.capacity
+        self.data[self.rear] = value
+
+    def dequeue(self):
+        if self.is_empty():
+            print("Queue is empty")
+            return None
+        value = self.data[self.front]
+        if self.front == self.rear:
+            self.front = -1
+            self.rear = -1
         else:
-            for i in range(self.front , len(self.list) , 1):
-                print(self.list[i])
-            for i in range(self.rear +1):
-                print(self.list[i])
+            self.front = (self.front + 1) % self.capacity
+        return value
 
-    def show_invalid(self):
-        if self.front == -1:
-            for i in range(len(self.list)):
-                print(self[i])
-                return
-            i = (self.rear+1) % len (self.list)
-            while i != self.front:
-                print(self.list[i])
-                i = (i +1) % len (self.list)
-
-    def Find(self , x):
+    def show_valid(self):
         if self.is_empty():
+            print("Queue is empty")
             return
         i = self.front
-        if self.list[i] == x:
-            return i
-        while i != self.rear:
-            i = (i +1) % len(self.list)
-            if self.list[i] == x:
+        while True:
+            print(self.data[i])
+            if i == self.rear:
+                break
+            i = (i + 1) % self.capacity
+
+    def find(self, x):
+        if self.is_empty():
+            return None
+        i = self.front
+        while True:
+            if self.data[i] == x:
                 return i
-            
-    def raplace(self , x , y):
+            if i == self.rear:
+                break
+            i = (i + 1) % self.capacity
+        return None
+
+    def replace(self, old, new):
         if self.is_empty():
             return
         i = self.front
-        if self.list[i] == x:
-            self.list[i] = y
-        while i != self.rear:
-            i = (i +1) % len (self.list)
-            if self.list[i] == x:
-                self.list[i] = y                   
-           
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
+        while True:
+            if self.data[i] == old:
+                self.data[i] = new
+            if i == self.rear:
+                break
+            i = (i + 1) % self.capacity
