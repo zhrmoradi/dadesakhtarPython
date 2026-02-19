6491fa61bef80a1f94da3d32f926d1d799196f95
zhrmoradi <zahra824264@gmail.com>
2026-02-19 13:33:13 +0330

    Update ds05.py

--- ds05.py
+++ ds05.py
@@ -1,47 +1,62 @@
-class dnode():
-    def __init__(self , x):
-        self.Data = x
+class DNode:
+    def __init__(self, data):
+        self.data = data
         self.next = None
-        self.back = None
+        self.prev = None
 
 
-class dlinked_list :
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
-    def del_after(self , x):
-        if self.head is None:
-            print("error")
-            return
-        c = self.head
-        while c:
-            if c.Data == x:
-                if c.next :
-                    a = c.next
-                    c.next = a.next
-                    if a.next:
-                        a.next.back = c
-                    del a
-                    return
-                print("error 1")
-                return
-            c = c.next
-            print("not found")
-    def del_x(self , x):
-        if self.head is None:
-            print("error")
-            return
-        c = self.head
-        while c:
-            if c.Data == x:
-                if c.next is None:
-                    self.del_last()
-                    return
-                c.back.next = c.next
-                c.next.back = c.back
-                del c
-                return
-            c = c.next
-            print("not found")
+        self.tail = None
 
-            
+    def append(self, data):
+        new_node = DNode(data)
+        if self.head is None:
+            self.head = self.tail = new_node
+            return
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
 
+    def delete_after(self, value):
+        current = self.head
+        while current:
+            if current.data == value:
+                if current.next:
+                    to_delete = current.next
+                    current.next = to_delete.next
+                    if to_delete.next:
+                        to_delete.next.prev = current
+                    else:
+                        self.tail = current
+                    del to_delete
+                    return True
+                return False
+            current = current.next
+        return False
+
+    def delete_value(self, value):
+        current = self.head
+        while current:
+            if current.data == value:
+                if current.prev:
+                    current.prev.next = current.next
+                else:
+                    self.head = current.next
+
+                if current.next:
+                    current.next.prev = current.prev
+                else:
+                    self.tail = current.prev
+
+                del current
+                return True
+            current = current.next
+        return False
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" <-> " if current.next else "\n")
+            current = current.next
