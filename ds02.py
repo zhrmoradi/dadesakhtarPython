3093445e07cebbcb986dc500eaff09888b917ae3
zhrmoradi <zahra824264@gmail.com>
2026-02-19 11:54:22 +0330

    Update ds02.py

--- ds02.py
+++ ds02.py
@@ -19,34 +19,24 @@ class stack :
         return self.st[-1]
          
 
-
-
-
-
 test = stack(10)
 test.push(57)
 test.push(126)
 test.push(-10)
 k=test.peek()
 
-
-
-
 #"ایندکس(x) های درون پشته را برگرداند."
 def find(self,x):
     for i in range(len(self.st)):
         if self.st[i] == x :
            print(i)
-
-
+            
 #"اولین شامل  (x) را برگرداند"
 def find1(self,x):
     for i in range(len(self.st)):
         if self.st[i] == x :
            print(i)
            return
-
-
 "اخرین ایندکس شامل (x) را چاپ کند"
 def find2(self,x):
     for i in range(len(self.st)-1,-1,-1) :
@@ -59,8 +49,6 @@ def find2_n(self,x):
             p=i
     print(p)                
 
-
-
 def find2_n(self,x):
     list=[]
     for i in range(len(self.st)):
@@ -68,12 +56,7 @@ def find2_n(self,x):
             list.append(i)
     print(list[2])
 
-
-
-
 def replace(self,x,y):
     for i in range(len(self.st)):
         if self.st[i] == x :
             self.st[i]=y
-                                
-
